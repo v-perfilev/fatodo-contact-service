6b3d2fb92ad99743303895d0d94ba6af200a7474
Vladimir Perfilev <persoff68@gmail.com>
2021-11-26 01:18:40 +0100

    TDM-112 fix code smells

--- src/main/java/com/persoff68/fatodo/web/rest/InfoController.java
+++ src/main/java/com/persoff68/fatodo/web/rest/InfoController.java
@@ -4,18 +4,12 @@ import com.persoff68.fatodo.model.dto.ContactInfoDTO;
 import com.persoff68.fatodo.security.exception.UnauthorizedException;
 import com.persoff68.fatodo.security.util.SecurityUtils;
 import com.persoff68.fatodo.service.InfoService;
-import com.persoff68.fatodo.service.RequestService;
-import com.persoff68.fatodo.web.rest.vm.RequestVM;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import java.util.UUID;
 
 @RestController
