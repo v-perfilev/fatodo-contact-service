bc13b9a90183adbfbd0312b89b3dd6dbfc63638a
Vladimir Perfilev <persoff68@gmail.com>
2021-12-21 09:15:03 +0300

    TDM-123 temporary disable cache | contact-service

--- src/main/java/com/persoff68/fatodo/repository/RelationRepository.java
+++ src/main/java/com/persoff68/fatodo/repository/RelationRepository.java
@@ -17,7 +17,7 @@ public interface RelationRepository extends JpaRepository<Relation, UUID> {
 
     int countByFirstUserId(UUID firstUserId);
 
-    @CacheableMethod(cacheName = "relations-by-id", keyCacheName = "relations-by-id-keys", key = "#firstUserId")
+//    @CacheableMethod(cacheName = "relations-by-id", keyCacheName = "relations-by-id-keys", key = "#firstUserId")
     List<Relation> findAllByFirstUserId(UUID firstUserId);
 
     @Query("SELECT r FROM Relation r WHERE (r.firstUserId = ?1 AND r.secondUserId = ?2)"
@@ -25,17 +25,17 @@ public interface RelationRepository extends JpaRepository<Relation, UUID> {
     List<Relation> findAllByUserIds(UUID firstUserId, UUID secondUserId);
 
     @Override
-    @MultiCacheEvictMethod({
-            @CacheEvictMethod(cacheName = "relations-by-id", key = "#contactRelationList.firstUserId"),
-            @CacheEvictMethod(cacheName = "relations-by-id", key = "#contactRelationList.secondUserId")
-    })
+//    @MultiCacheEvictMethod({
+//            @CacheEvictMethod(cacheName = "relations-by-id", key = "#contactRelationList.firstUserId"),
+//            @CacheEvictMethod(cacheName = "relations-by-id", key = "#contactRelationList.secondUserId")
+//    })
     @NonNull
     <S extends Relation> List<S> saveAll(@NonNull Iterable<S> contactRelationList);
 
     @Override
-    @MultiCacheEvictMethod({
-            @CacheEvictMethod(cacheName = "relations-by-id", key = "#contactRelationList.firstUserId"),
-            @CacheEvictMethod(cacheName = "relations-by-id", key = "#contactRelationList.secondUserId")
-    })
+//    @MultiCacheEvictMethod({
+//            @CacheEvictMethod(cacheName = "relations-by-id", key = "#contactRelationList.firstUserId"),
+//            @CacheEvictMethod(cacheName = "relations-by-id", key = "#contactRelationList.secondUserId")
+//    })
     void deleteAll(@NonNull Iterable<? extends Relation> contactRelationList);
 }
