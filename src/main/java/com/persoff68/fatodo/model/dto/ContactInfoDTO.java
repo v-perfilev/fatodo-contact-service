cf41a44ba32cd084a5a0f33ba53e3ffe21bff045
Vladimir Perfilev <persoff68@gmail.com>
2022-07-15 14:20:12 +0200

    TDM-149 delete serializable from classes

--- src/main/java/com/persoff68/fatodo/model/dto/ContactInfoDTO.java
+++ src/main/java/com/persoff68/fatodo/model/dto/ContactInfoDTO.java
@@ -1,19 +1,13 @@
 package com.persoff68.fatodo.model.dto;
 
-import com.persoff68.fatodo.config.constant.AppConstants;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.io.Serial;
-import java.io.Serializable;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ContactInfoDTO implements Serializable {
-    @Serial
-    private static final long serialVersionUID = AppConstants.SERIAL_VERSION_UID;
+public class ContactInfoDTO {
 
     private int relationCount;
     private int outcomingRequestCount;
--- src/main/java/com/persoff68/fatodo/model/vm/RequestVM.java
+++ src/main/java/com/persoff68/fatodo/model/vm/RequestVM.java
@@ -1,21 +1,16 @@
 package com.persoff68.fatodo.model.vm;
 
-import com.persoff68.fatodo.config.constant.AppConstants;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
-import java.io.Serial;
-import java.io.Serializable;
 import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class RequestVM implements Serializable {
-    @Serial
-    private static final long serialVersionUID = AppConstants.SERIAL_VERSION_UID;
+public class RequestVM {
 
     @NotNull
     private UUID recipientId;
