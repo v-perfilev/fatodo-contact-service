e9edf5e712c28c841f31af3883484559ec4fcb45
Vladimir Perfilev <persoff68@gmail.com>
2022-07-15 12:06:39 +0200

    TDM-149 put in order serializable classes

--- src/main/java/com/persoff68/fatodo/model/dto/ContactInfoDTO.java
+++ src/main/java/com/persoff68/fatodo/model/dto/ContactInfoDTO.java
@@ -1,13 +1,19 @@
 package com.persoff68.fatodo.model.dto;
 
+import com.persoff68.fatodo.config.constant.AppConstants;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ContactInfoDTO {
+public class ContactInfoDTO implements Serializable {
+    @Serial
+    private static final long serialVersionUID = AppConstants.SERIAL_VERSION_UID;
 
     private int relationCount;
     private int outcomingRequestCount;
--- src/main/java/com/persoff68/fatodo/model/dto/RequestDTO.java
+++ src/main/java/com/persoff68/fatodo/model/dto/RequestDTO.java
@@ -1,10 +1,12 @@
 package com.persoff68.fatodo.model.dto;
 
+import com.persoff68.fatodo.config.constant.AppConstants;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
 import java.util.UUID;
 
 @Data
@@ -12,6 +14,8 @@ import java.util.UUID;
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class RequestDTO extends AbstractAuditingDTO {
+    @Serial
+    private static final long serialVersionUID = AppConstants.SERIAL_VERSION_UID;
 
     private UUID requesterId;
     private UUID recipientId;
