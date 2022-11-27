d47a5abb585f2d9525b64528d5fb5bd7204c57c7
Vladimir Perfilev <persoff68@gmail.com>
2022-11-27 15:27:35 +0100

    TDM-166 revert cachable in RelationRepository

--- src/main/java/com/persoff68/fatodo/repository/RelationRepository.java
+++ src/main/java/com/persoff68/fatodo/repository/RelationRepository.java
@@ -1,8 +1,11 @@
 package com.persoff68.fatodo.repository;
 
+import com.persoff68.fatodo.config.aop.cache.annotation.CacheEvictMethod;
+import com.persoff68.fatodo.config.aop.cache.annotation.CacheableMethod;
 import com.persoff68.fatodo.model.Relation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,10 +16,21 @@ public interface RelationRepository extends JpaRepository<Relation, UUID> {
 
     int countByFirstUserId(UUID firstUserId);
 
+    @CacheableMethod(cacheName = "relations-by-id", key = "#firstUserId")
     List<Relation> findAllByFirstUserId(UUID firstUserId);
 
     @Query("SELECT r FROM Relation r WHERE (r.firstUserId = ?1 AND r.secondUserId = ?2)"
             + " or (r.firstUserId = ?2 AND r.secondUserId = ?1)")
     List<Relation> findAllByUserIds(UUID firstUserId, UUID secondUserId);
 
+    @Override
+    @CacheEvictMethod(cacheName = "relations-by-id", key = "#contactRelationList.firstUserId")
+    @CacheEvictMethod(cacheName = "relations-by-id", key = "#contactRelationList.secondUserId")
+    @NonNull
+    <S extends Relation> List<S> saveAll(@NonNull Iterable<S> contactRelationList);
+
+    @Override
+    @CacheEvictMethod(cacheName = "relations-by-id", key = "#contactRelationList.firstUserId")
+    @CacheEvictMethod(cacheName = "relations-by-id", key = "#contactRelationList.secondUserId")
+    void deleteAll(@NonNull Iterable<? extends Relation> contactRelationList);
 }
